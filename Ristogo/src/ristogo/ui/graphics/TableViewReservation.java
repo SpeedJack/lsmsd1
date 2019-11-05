ba61ffa7a683c6b3077a00625d9d1c0fe4975a84
Niccolò Scatena <speedjack95@gmail.com>
2019-11-05 11:43:33 +0100

    Fix all warnings

--- Ristogo/src/ristogo/ui/graphics/TableViewReservation.java
+++ Ristogo/src/ristogo/ui/graphics/TableViewReservation.java
@@ -8,65 +8,71 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import ristogo.config.Configuration;
 import ristogo.ui.graphics.beans.ReservationBean;
 
-public class TableViewReservation extends TableView<ReservationBean> {
-	
-	 private final ObservableList<ReservationBean> reservationList;
-	 private TableColumn nameColumn;
-	 private TableColumn dateColumn;
-	 private TableColumn hourColumn;
-	 private TableColumn seatsColumn;
-	 
-	 
-	  public TableViewReservation (boolean isCustomer) {
-		  
-		  	String font = Configuration.getConfig().getFontName();
-			double dimC = Configuration.getConfig().getFontSize();
-			String textColor = Configuration.getConfig().getFgColorName();
-		  
-		    setEditable(false);
-		    setFixedCellSize(35);
-		    setMinWidth(600);
-		    setMaxWidth(600);
-		    
-		    if(isCustomer) {
-		    	setMaxHeight((Configuration.getConfig().getnumberRowsDisplayable()-2)*getFixedCellSize());
-		    }
-		    else {
-		    	setMaxHeight(Configuration.getConfig().getnumberRowsDisplayable()*getFixedCellSize());
-		    }
-		  
-		    nameColumn = new TableColumn("Name");
-		    if(isCustomer) {
-		    	
-		    	nameColumn.setCellValueFactory(new PropertyValueFactory<>("restaurantName"));
-		    }
-		    else {
-		    	nameColumn.setCellValueFactory(new PropertyValueFactory<>("clientName"));
-		    }
-		    nameColumn.setStyle("-fx-font-family: " + font + "; -fx-font-size: " + dimC + "; -fx-color: "+ textColor +";");
-		    
-		    
-		    dateColumn = new TableColumn("Date");
-		    dateColumn.setCellValueFactory(new PropertyValueFactory<>("date"));
-		    dateColumn.setStyle(" -fx-alignment: CENTER; -fx-font-family: " + font + "; -fx-font-size: " + dimC+ "; -fx-color: "+ textColor +";");
-		    
-		    hourColumn = new TableColumn("Hour");
-		    hourColumn.setCellValueFactory(new PropertyValueFactory<>("hour"));
-		    hourColumn.setStyle("-fx-alignment: CENTER; -fx-font-family: " + font + "; -fx-font-size: " + dimC + "; -fx-color: "+ textColor +";");
-		    
-		    seatsColumn = new TableColumn("Seats");
-		    seatsColumn.setCellValueFactory(new PropertyValueFactory<>("seats"));
-		    seatsColumn.setStyle("-fx-alignment: CENTER; -fx-font-family: " + font + "; -fx-font-size: " + dimC + "; -fx-color: "+ textColor +";");
-		    
-		    nameColumn.setMinWidth(300); nameColumn.setMaxWidth(300);
-		    dateColumn.setMinWidth(150); dateColumn.setMaxWidth(150);
-		    hourColumn.setMinWidth(100); hourColumn.setMaxWidth(100);
-		    seatsColumn.setMinWidth(50); seatsColumn.setMaxWidth(50);
+public class TableViewReservation extends TableView<ReservationBean>
+{
 
-		   
-		    getColumns().addAll(nameColumn, dateColumn, hourColumn, seatsColumn);
-		    reservationList = FXCollections.observableArrayList();
-		    setItems(reservationList); 
-		  }  
+	private final ObservableList<ReservationBean> reservationList;
+	private TableColumn<ReservationBean, String> nameColumn;
+	private TableColumn<ReservationBean, String> dateColumn;
+	private TableColumn<ReservationBean, String> hourColumn;
+	private TableColumn<ReservationBean, String> seatsColumn;
+
+	@SuppressWarnings("unchecked")
+	public TableViewReservation(boolean isCustomer)
+	{
+
+		String font = Configuration.getConfig().getFontName();
+		double dimC = Configuration.getConfig().getFontSize();
+		String textColor = Configuration.getConfig().getFgColorName();
+
+		setEditable(false);
+		setFixedCellSize(35);
+		setMinWidth(600);
+		setMaxWidth(600);
+
+		if (isCustomer) {
+			setMaxHeight((Configuration.getConfig().getnumberRowsDisplayable() - 2) * getFixedCellSize());
+		} else {
+			setMaxHeight(Configuration.getConfig().getnumberRowsDisplayable() * getFixedCellSize());
+		}
+
+		nameColumn = new TableColumn<ReservationBean, String>("Name");
+		if (isCustomer) {
+
+			nameColumn.setCellValueFactory(new PropertyValueFactory<>("restaurantName"));
+		} else {
+			nameColumn.setCellValueFactory(new PropertyValueFactory<>("clientName"));
+		}
+		nameColumn.setStyle(
+			"-fx-font-family: " + font + "; -fx-font-size: " + dimC + "; -fx-color: " + textColor + ";");
+
+		dateColumn = new TableColumn<ReservationBean, String>("Date");
+		dateColumn.setCellValueFactory(new PropertyValueFactory<>("date"));
+		dateColumn.setStyle(" -fx-alignment: CENTER; -fx-font-family: " + font + "; -fx-font-size: " + dimC +
+			"; -fx-color: " + textColor + ";");
+
+		hourColumn = new TableColumn<ReservationBean, String>("Hour");
+		hourColumn.setCellValueFactory(new PropertyValueFactory<>("hour"));
+		hourColumn.setStyle("-fx-alignment: CENTER; -fx-font-family: " + font + "; -fx-font-size: " + dimC +
+			"; -fx-color: " + textColor + ";");
+
+		seatsColumn = new TableColumn<ReservationBean, String>("Seats");
+		seatsColumn.setCellValueFactory(new PropertyValueFactory<>("seats"));
+		seatsColumn.setStyle("-fx-alignment: CENTER; -fx-font-family: " + font + "; -fx-font-size: " + dimC +
+			"; -fx-color: " + textColor + ";");
+
+		nameColumn.setMinWidth(300);
+		nameColumn.setMaxWidth(300);
+		dateColumn.setMinWidth(150);
+		dateColumn.setMaxWidth(150);
+		hourColumn.setMinWidth(100);
+		hourColumn.setMaxWidth(100);
+		seatsColumn.setMinWidth(50);
+		seatsColumn.setMaxWidth(50);
+
+		getColumns().addAll(nameColumn, dateColumn, hourColumn, seatsColumn);
+		reservationList = FXCollections.observableArrayList();
+		setItems(reservationList);
+	}
 
 }
--- Ristogo/src/ristogo/ui/graphics/TableViewRestaurant.java
+++ Ristogo/src/ristogo/ui/graphics/TableViewRestaurant.java
@@ -1,6 +1,5 @@
 package ristogo.ui.graphics;
 
-
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.TableColumn;
@@ -9,61 +8,66 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import ristogo.config.Configuration;
 import ristogo.ui.graphics.beans.RestaurantBean;
 
+public class TableViewRestaurant extends TableView<RestaurantBean>
+{
+	final ObservableList<RestaurantBean> restaurantList;
+	private TableColumn<RestaurantBean, String> nameColumn;
+	private TableColumn<RestaurantBean, String> typeColumn;
+	private TableColumn<RestaurantBean, String> priceColumn;
+	private TableColumn<RestaurantBean, String> cityColumn;
+	private TableColumn<RestaurantBean, String> addressColumn;
 
+	@SuppressWarnings("unchecked")
+	public TableViewRestaurant()
+	{
+		setEditable(true);
+		setFixedCellSize(35);
+		setMaxHeight(Configuration.getConfig().getnumberRowsDisplayable() * getFixedCellSize());
+		setMinWidth(600);
+		setMaxWidth(600);
+		String font = Configuration.getConfig().getFontName();
+		double dimC = Configuration.getConfig().getFontSize();
+		String textColor = Configuration.getConfig().getFgColorName();
 
-public class TableViewRestaurant extends TableView<RestaurantBean> {
-	 final ObservableList<RestaurantBean> restaurantList;
-	 private TableColumn nameColumn;
-	 private TableColumn typeColumn;
-	 private TableColumn priceColumn;
-	 private TableColumn cityColumn;
-	 private TableColumn addressColumn;
-	 
-		 
-	  public TableViewRestaurant () {
-		    setEditable(true);
-		    setFixedCellSize(35);
-		    setMaxHeight(Configuration.getConfig().getnumberRowsDisplayable()*getFixedCellSize());
-		    setMinWidth(600);
-		    setMaxWidth(600);
-			String font = Configuration.getConfig().getFontName();
-			double dimC = Configuration.getConfig().getFontSize();
-			String textColor = Configuration.getConfig().getFgColorName();
-		    
-		    
-		    nameColumn = new TableColumn("Name");
-		    nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
-		    nameColumn.setStyle("-fx-font-family: " + font + "; -fx-font-size: " + dimC + "; -fx-color: "+ textColor +";");
-		    
-		    
-		    typeColumn = new TableColumn("Type");
-		    typeColumn.setCellValueFactory(new PropertyValueFactory<>("type"));
-		    typeColumn.setStyle(" -fx-alignment: CENTER; -fx-font-family: " + font + "; -fx-font-size: " + dimC + "; -fx-color: "+ textColor +";");
-		    
-		    priceColumn = new TableColumn("Price");
-		    priceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
-		    priceColumn.setStyle("-fx-alignment: CENTER; -fx-font-family: " + font + "; -fx-font-size: " + dimC + "; -fx-color: "+ textColor +";");
-		    
-		    cityColumn = new TableColumn("City");
-		    cityColumn.setCellValueFactory(new PropertyValueFactory<>("city"));
-		    cityColumn.setStyle("-fx-alignment: CENTER; -fx-font-family: " + font + "; -fx-font-size: " + dimC + "; -fx-color: "+ textColor +";");
-		    
-		    
-		    addressColumn = new TableColumn("Address");
-		    addressColumn.setCellValueFactory(new PropertyValueFactory<>("address"));
-		    addressColumn.setStyle("-fx-alignment: CENTER; -fx-font-family: " + font + "; -fx-font-size: " + dimC + "; -fx-color: "+ textColor +";");
-		    
-		    
-		    nameColumn.setMinWidth(150); nameColumn.setMaxWidth(150);
-		    typeColumn.setMinWidth(100); typeColumn.setMaxWidth(100);
-		    priceColumn.setMinWidth(50); priceColumn.setMaxWidth(50);
-		    cityColumn.setMinWidth(100); cityColumn.setMaxWidth(100);
-		    addressColumn.setMinWidth(200); addressColumn.setMaxWidth(200);
+		nameColumn = new TableColumn<RestaurantBean, String>("Name");
+		nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+		nameColumn.setStyle(
+			"-fx-font-family: " + font + "; -fx-font-size: " + dimC + "; -fx-color: " + textColor + ";");
 
-		    
-		    getColumns().addAll(nameColumn, typeColumn, priceColumn, cityColumn, addressColumn);
-		    restaurantList = FXCollections.observableArrayList();
-		    setItems(restaurantList);
-		    
-		  }	 
+		typeColumn = new TableColumn<RestaurantBean, String>("Type");
+		typeColumn.setCellValueFactory(new PropertyValueFactory<>("type"));
+		typeColumn.setStyle(" -fx-alignment: CENTER; -fx-font-family: " + font + "; -fx-font-size: " + dimC +
+			"; -fx-color: " + textColor + ";");
+
+		priceColumn = new TableColumn<RestaurantBean, String>("Price");
+		priceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
+		priceColumn.setStyle("-fx-alignment: CENTER; -fx-font-family: " + font + "; -fx-font-size: " + dimC +
+			"; -fx-color: " + textColor + ";");
+
+		cityColumn = new TableColumn<RestaurantBean, String>("City");
+		cityColumn.setCellValueFactory(new PropertyValueFactory<>("city"));
+		cityColumn.setStyle("-fx-alignment: CENTER; -fx-font-family: " + font + "; -fx-font-size: " + dimC +
+			"; -fx-color: " + textColor + ";");
+
+		addressColumn = new TableColumn<RestaurantBean, String>("Address");
+		addressColumn.setCellValueFactory(new PropertyValueFactory<>("address"));
+		addressColumn.setStyle("-fx-alignment: CENTER; -fx-font-family: " + font + "; -fx-font-size: " + dimC +
+			"; -fx-color: " + textColor + ";");
+
+		nameColumn.setMinWidth(150);
+		nameColumn.setMaxWidth(150);
+		typeColumn.setMinWidth(100);
+		typeColumn.setMaxWidth(100);
+		priceColumn.setMinWidth(50);
+		priceColumn.setMaxWidth(50);
+		cityColumn.setMinWidth(100);
+		cityColumn.setMaxWidth(100);
+		addressColumn.setMinWidth(200);
+		addressColumn.setMaxWidth(200);
+
+		getColumns().addAll(nameColumn, typeColumn, priceColumn, cityColumn, addressColumn);
+		restaurantList = FXCollections.observableArrayList();
+		setItems(restaurantList);
+
+	}
 }
