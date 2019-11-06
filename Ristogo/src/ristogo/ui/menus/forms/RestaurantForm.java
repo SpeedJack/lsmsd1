0ea5a999b11b784a8dfe04da7613802b8bbaa4d0
Niccolò Scatena <speedjack95@gmail.com>
2019-11-06 16:11:56 +0100

    Use lambda functions for form validation

--- Ristogo/src/ristogo/ui/menus/forms/RestaurantForm.java
+++ Ristogo/src/ristogo/ui/menus/forms/RestaurantForm.java
@@ -20,19 +20,14 @@ public class RestaurantForm extends TextForm
 	protected LinkedHashSet<FormField> createFields()
 	{
 		LinkedHashSet<FormField> fields = new LinkedHashSet<FormField>();
-		fields.add(new FormField("Name", restaurant.getName(), this::validateString));
-		fields.add(new FormField("Genre", restaurant.getGenre(), this::validateString));
+		fields.add(new FormField("Name", restaurant.getName(), (s) -> { return s.length() < 33; }));
+		fields.add(new FormField("Genre", restaurant.getGenre(), (s) -> { return s.length() < 33; }));
 		fields.add(new ChoiceFormField("Price", restaurant.getPrice().toString(), Price.class));
-		fields.add(new FormField("City", restaurant.getCity(), this::validateString));
-		fields.add(new FormField("Address", restaurant.getAddress(), this::validateString));
+		fields.add(new FormField("City", restaurant.getCity(), (s) -> { return s.length() < 33; }));
+		fields.add(new FormField("Address", restaurant.getAddress(), (s) -> { return s.length() < 33; }));
 		fields.add(new FormField("Description", restaurant.getDescription()));
 		fields.add(new ChoiceFormField("Opening hours", restaurant.getOpeningHours().toString(), OpeningHours.class));
 		return fields;
-	}
-	
-	private boolean validateString(String str)
-	{
-		return !str.isBlank() && str.length() < 33;
 	}
 	
 	/*private boolean validatePrice(String price)
