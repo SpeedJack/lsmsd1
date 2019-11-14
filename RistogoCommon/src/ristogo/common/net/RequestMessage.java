d12b6a21170404bc2d79e2ac79549d0e39cdcbfa
Niccolò Scatena <speedjack95@gmail.com>
2019-11-14 19:14:07 +0100

    Change visibility of action field

--- RistogoCommon/src/ristogo/common/net/RequestMessage.java
+++ RistogoCommon/src/ristogo/common/net/RequestMessage.java
@@ -11,7 +11,7 @@ public class RequestMessage extends Message
 {
 	private static final long serialVersionUID = 6989601732466426604L;
 
-	private final ActionRequest action;
+	protected final ActionRequest action;
 	
 	public RequestMessage(ActionRequest action, Entity... entities)
 	{
